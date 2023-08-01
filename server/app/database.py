f4789650db4a59372ad7f6957d9669be01dc4cd4
mkilic20 <mkilic20@ku.edu.tr>
2023-08-01 15:27:21 +0300

    env variables functioning except root

--- server/app/database.py
+++ server/app/database.py
@@ -2,10 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import time
+import os
 
-DATABASE_URL = "mysql://root:pass@db/mydb"
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+host = os.getenv("HOST", "db") 
+database = os.getenv("MYSQL_DATABASE")
+port = os.getenv("MYSQL_PORT", 3306)
 
-time.sleep(10)
+DATABASE_URL = f"mysql://root:{password}@{host}/{database}"
+
+#DATABASE_URL = "mysql://root:2002@db/mydb"
+
+time.sleep(5)
 
 engine = create_engine(DATABASE_URL,echo=True)
 
