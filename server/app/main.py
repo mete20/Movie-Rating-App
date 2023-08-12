d8633f2d36d3d8e6442f913b61c749cd8689041b
Lab <lab@ENG-B50-10.local>
2023-08-12 19:22:23 +0300

    auth.py, jwt.py, main.py updated to use OAuth2 and related features

--- server/app/main.py
+++ server/app/main.py
@@ -2,74 +2,43 @@ from . import crud, models, schemas
 from .database import SessionLocal, engine
 from typing import List, Annotated
 from fastapi import Depends, FastAPI, HTTPException, Request, status
+from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 import secrets
-import os
-from starlette.config import Config
-from starlette.middleware.sessions import SessionMiddleware
-from starlette.responses import HTMLResponse
-from starlette.responses import RedirectResponse
-from authlib.integrations.starlette_client import OAuth
-from authlib.integrations.starlette_client import OAuthError
 
+from app.auth import auth_app
 
 app = FastAPI()
 
-# OAuth settings
-
-GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or None
-GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or None
-
-if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
-    raise BaseException('Missing env variables')
-
-# Set up OAuth
-
-config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
-starlette_config = Config(environ=config_data)
-oauth = OAuth(starlette_config)
-oauth.register(
-    name='google',
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    cleint_kwargs={'scope': 'openid email profile'},
-)
-
-#Set up middleware to read the request session
-SECRET_KEY= os.environ.get('SECRET_KEY') or None
-if SECRET_KEY is None:
-    raise BaseException('Missing SECRET_KEY')
-app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+app.mount('/auth', auth_app)
 
 @app.get('/')
-def public(request: Request):
-    user = request.session.get('user')
-    if user:
-        name = user.get('name')
-        return HTMLResponse(f'<p>Hello {name}!</p><a href=/logout>Logout</a>')
-    return HTMLResponse('<a href=/login>Login</a>')
+async def root():
+    return HTMLResponse('<body><a href="/auth/login">Log In</a></body>')
 
-@app.route('/logout/')
-async def logout(request: Request):
-    request.session.pop('user', None)
-    return RedirectResponse(url='/')
+@app.get('/token')
+async def token(request: Request):
+    return HTMLResponse('''
+                <script>
+                function send(){
+                    var req = new XMLHttpRequest();
+                    req.onreadystatechange = function() {
+                        if (req.readyState === 4) {
+                            console.log(req.response);
+                            if (req.response["result"] === true) {
+                                window.localStorage.setItem('jwt', req.response["access_token"]);
+                            }
+                        }
+                    }
+                    req.withCredentials = true;
+                    req.responseType = 'json';
+                    req.open("get", "/auth/token?"+window.location.search.substr(1), true);
+                    req.send("");
 
-
-@app.route('/login/')
-async def login(request: Request):
-    redirect_uri = request.url_for('auth')  # This creates the url for our /auth endpoint
-    return await oauth.google.authorize_redirect(request, redirect_uri)
-
-
-@app.route('/auth/')
-async def auth(request: Request):
-    try:
-        access_token = await oauth.google.authorize_access_token(request)
-    except OAuthError:
-        return RedirectResponse(url='/')
-    user_data = await oauth.google.parse_id_token(request, access_token)
-    request.session['user'] = dict(user_data)
-    return RedirectResponse(url='/')
-
+                }
+                </script>
+                <button onClick="send()">Get FastAPI JWT Token</button>
+            ''')
 
 # Dependency
 def get_db():
