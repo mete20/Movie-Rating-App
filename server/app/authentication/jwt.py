b9763ae74966335d15696c21914cad3fd13f9307
mkilic20 <mkilic20@ku.edu.tr>
2023-08-29 09:42:42 +0300

    fields added

--- server/app/authentication/jwt.py
+++ server/app/authentication/jwt.py
@@ -7,27 +7,14 @@ from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from app.config import Config
 
 http_bearer = HTTPBearer()
 
-# Helper to read numbers using var envs
-def cast_to_number(id):
-    temp = os.environ.get(id)
-    if temp is not None:
-        try:
-            return float(temp)
-        except ValueError:
-            return None
-    return None
-
 
 # Configuration
-API_SECRET_KEY = os.environ.get('API_SECRET_KEY') or None
-if API_SECRET_KEY is None:
+if Config.API_SECRET_KEY is None:
     raise BaseException('Missing API_SECRET_KEY env var.')
-API_ALGORITHM = os.environ.get('API_ALGORITHM') or 'HS256'
-API_ACCESS_TOKEN_EXPIRE_MINUTES = cast_to_number('API_ACCESS_TOKEN_EXPIRE_MINUTES') or 15
-REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 30
 
 
 # Error
@@ -46,13 +33,13 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({'exp': expire})
-    encoded_jwt = jwt.encode(to_encode, API_SECRET_KEY, algorithm=API_ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, Config.API_SECRET_KEY, algorithm=Config.API_ALGORITHM)
     return encoded_jwt
 
 
 # Create token for an email
 def create_token(email):
-    access_token_expires = timedelta(minutes=API_ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=Config.API_ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={'sub': email}, expires_delta=access_token_expires)
     return access_token
 
@@ -82,7 +69,7 @@ async def is_admin_dep(token: Annotated[str, HTTPAuthorizationCredentials] = Dep
 
 async def get_current_user_email(token: Annotated[str, HTTPAuthorizationCredentials] = Depends(http_bearer)):
     try:
-        payload = jwt.decode(token.credentials, API_SECRET_KEY, algorithms=[API_ALGORITHM])
+        payload = jwt.decode(token.credentials, Config.API_SECRET_KEY, algorithms=[Config.API_ALGORITHM])
         email: str = payload.get('sub')
         if email is None:
             raise CREDENTIALS_EXCEPTION
@@ -93,11 +80,11 @@ async def get_current_user_email(token: Annotated[str, HTTPAuthorizationCredenti
 
 
 def create_refresh_token(email):
-    expires = timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+    expires = timedelta(minutes=Config.REFRESH_TOKEN_EXPIRE_MINUTES)
     return create_access_token(data={'sub': email}, expires_delta=expires)
 
 def decode_token(token):
-    return jwt.decode(token, API_SECRET_KEY, algorithms=[API_ALGORITHM])
+    return jwt.decode(token, Config.API_SECRET_KEY, algorithms=[Config.API_ALGORITHM])
 
 async def get_current_user_token(token: str = Depends(http_bearer)):
     _ = await get_current_user_email(token)
