fbb8b996ed2953ecc978922b3c02e1cb7efd1159
mkilic20 <mkilic20@ku.edu.tr>
2023-09-01 14:29:14 +0300

    more movie test cases & user delete

--- client/app/testing/main.py
+++ client/app/testing/main.py
@@ -7,17 +7,17 @@ import os
 SERVER_URL = "http://web:8000"
 AUTH_ROUTE = "/auth/token"
 MOVIES_ROUTE = "/movies"
+USERS_ROUTE = "/users"
 API_SECRET_KEY = os.environ['API_SECRET_KEY']
 API_ALGORITHM = os.environ['API_ALGORITHM']
 
-
+#Authentication and Authorization
 def mock_authorize(email: str):
     """
     Mock the authorization process for a given email.
     :param email: The email to be authorized.
     :return: A dictionary containing the access token.
     """
-    # This function directly generates tokens without actual authentication for testing purposes.
     expire_time = datetime.utcnow() + timedelta(minutes=15)
     user_info = {'sub': email, 'exp': expire_time}
     access_token = jwt.encode(user_info, API_SECRET_KEY, algorithm=API_ALGORITHM)
@@ -25,17 +25,22 @@ def mock_authorize(email: str):
         'access_token': access_token,
     }
 
-def mock_authenticate():
-    # This function simulates the authentication process by directly mocking the authorization for a given email.
-    # For this mock, the email "test@ku.edu.tr" is used for testing purposes.
+
+def mock_authenticate_admin():
     tokens = mock_authorize("test@ku.edu.tr")
     return tokens.get("access_token")
 
-# Get Movies Function
-def get_movies(token):
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(f"{SERVER_URL}{MOVIES_ROUTE}", headers=headers)
+
+def mock_authenticate_user():
+    tokens = mock_authorize("test@gmail.com")
+    return tokens.get("access_token")
+
+
+# Movie Requests
+def get_movies():
+    response = requests.get(f"{SERVER_URL}{MOVIES_ROUTE}")
     return response.json()
+
 
 def create_movie(token: str, movie: dict) -> dict:
     headers = {"Authorization": f"Bearer {token}"}
@@ -46,14 +51,15 @@ def create_movie(token: str, movie: dict) -> dict:
     else:
         response.raise_for_status()
 
-def get_movie_by_id(token: str, movie_id: int) -> dict:
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(f"{SERVER_URL}{MOVIES_ROUTE}/{movie_id}", headers=headers)
+
+def get_movie_by_id(movie_id: int) -> dict:
+    response = requests.get(f"{SERVER_URL}{MOVIES_ROUTE}/{movie_id}")
     
     if response.status_code == 200:
         return response.json()
     else:
         response.raise_for_status()
+   
         
 def delete_movie(token: str, movie_id: int) -> dict:
     headers = {"Authorization": f"Bearer {token}"}
@@ -64,9 +70,40 @@ def delete_movie(token: str, movie_id: int) -> dict:
     else:
         response.raise_for_status()
 
-# Example usage
-if __name__ == "__main__":
-    token = mock_authenticate()
-    movies = get_movies(token)
-    print(movies)
+# User Requests
 
+def create_user(token: str, user: dict) -> dict:
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.post(f"{SERVER_URL}{USERS_ROUTE}/", headers=headers, json=user)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+def get_users(token: str) -> list:
+    response = requests.get(f"{SERVER_URL}{USERS_ROUTE}/")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+def get_user_by_id(token: str, user_id: int) -> dict:
+    response = requests.get(f"{SERVER_URL}{USERS_ROUTE}/{user_id}")
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+def delete_user(token: str, user_id: int) -> dict:
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.delete(f"{SERVER_URL}{USERS_ROUTE}/{user_id}", headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
--- server/app/routers/router_user.py
+++ server/app/routers/router_user.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.crud import crud_user
 from app.schemas import schema_user
-from app.db.database import SessionLocal
 from app.db.database import get_db
 from app.authentication.jwt import get_current_user_email, is_admin_dep
 from typing import List
@@ -16,7 +15,6 @@ router = APIRouter(
 
 @router.post("/", response_model=schema_user.User)
 def create_user(user: schema_user.UserCreate, db: Session = Depends(get_db), is_admin: bool = Depends(is_admin_dep)):
-    print(user)
     db_user = crud_user.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(
@@ -42,6 +40,20 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
             status_code=404,
             detail={
                 "error code:": 404,
+                "error description": "Not Found",
+                "message": f"User with ID: '{user_id}' not found",
+            },
+        )
+    return db_user
+
+@router.delete("/{user_id}", response_model=schema_user.User)
+def delete_user(user_id: int, db: Session = Depends(get_db), is_admin: bool = Depends(is_admin_dep)):
+    db_user = crud_user.delete_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail={
+                "error code": 404,
                 "error description": "Not Found",
                 "message": f"User with ID: '{user_id}' not found",
             },
